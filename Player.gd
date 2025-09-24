0b21d9384e4e577bdb2acddd162c9014226d11f0
Rasnomawan <alvinorasya9@gmail.com>
2025-09-24 09:25:22 +0700

    penamabahan art gambar

--- Player.gd
+++ Player.gd
@@ -2,14 +2,9 @@ extends KinematicBody2D
 
 export var speed := 200
 export var jump_force := 400
-export var gravity := 800
-export var coyote_time := 0.15     # waktu toleransi setelah jatuh
-export var jump_buffer_time := 0.15 # waktu toleransi sebelum menyentuh lantai
+export var gravity := 900
 
 var velocity = Vector2.ZERO
-var coyote_timer = 0.0
-var jump_buffer_timer = 0.0
-
 onready var sprite = $Sprite
 
 func _physics_process(delta):
@@ -17,13 +12,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
-		coyote_timer = coyote_time  # reset coyote time kalau nyentuh lantai
-
-	# Kurangi timer
-	if coyote_timer > 0:
-		coyote_timer -= delta
-	if jump_buffer_timer > 0:
-		jump_buffer_timer -= delta
+		velocity.y = 0
 
 	# Input kiri/kanan
 	var input_dir = 0
@@ -37,15 +26,9 @@ func _physics_process(delta):
 	if input_dir != 0:
 		sprite.flip_h = input_dir < 0
 
-	# Input lompat → buffer
-	if Input.is_action_just_pressed("ui_accept"):
-		jump_buffer_timer = jump_buffer_time
-
-	# Lompat hanya kalau masih dalam coyote time DAN ada buffer input
-	if jump_buffer_timer > 0 and coyote_timer > 0:
+	# Lompat
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = -jump_force
-		jump_buffer_timer = 0
-		coyote_timer = 0
 
 	# Gerakkan player
 	velocity = move_and_slide(velocity, Vector2.UP)
